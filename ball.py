3c1d2a95cdfb0f83c16257db84e593b2aea70fe1
Viggo C <viggo.carrefors@gmail.com>
2024-10-10 10:34:15 +0200

    progress

--- ball.py
+++ ball.py
@@ -3,4 +3,26 @@ from turtle import Turtle
 class Ball(Turtle):
     
     def __init__(self):
-        pass
+        super().__init__()
+        self.increase = 0
+        self.shape("circle")
+        self.speed(10 + self.increase)
+        self.penup()
+        self.color("white")
+        self.start_direction = True
+        self.start(True)
+    
+    def bounce(self):
+        if self.start_direction == True:
+            self.left(90)
+        else:
+            self.right(90)
+        self.increase += 5
+        
+    def start(self, direction):
+        self.increase = 0
+        self.start_direction = direction
+        if direction == True:
+            self.setheading(45)
+        else:
+            self.setheading(225)
--- paddle.py
+++ paddle.py
@@ -1,5 +1,35 @@
 from turtle import Turtle
 
 class Paddle(Turtle):
+    
+    MOVE_DISTANCE = 10
+    
     def __init__(self, position):
-        pass
+        id += 1
+        super().__init__()
+        self.body = []
+        self.position = position
+        self.initialize()
+    
+    def initialize(self):
+        y = 0
+        for _ in range(3):
+            self.add_part(y)
+            y -= 10
+    
+    def add_part(self, y):
+        temp_turtle = Turtle(shape = "square")
+        temp_turtle.color("white")
+        temp_turtle.penup()
+        temp_turtle.goto(self.position[0], self.position[1] - y)
+        self.body.append(temp_turtle)
+        
+    def move(self, direction):
+        if direction == True:
+            self.setheading(90)
+            for i in self.body:
+                i.forward(self.MOVE_DISTANCE)
+        else:
+            self.setheading(270)
+            for i in self.body:
+                i.forward(self.MOVE_DISTANCE)
